13c28a4634f59e7bd229125fa2315fcb7565241a
yoqubjonxon <bakhronovfirdavs@gmail.com>
2026-05-18 05:05:31 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+var CACHE = 'ps5klub-v1';
+var FILES = ['./index.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(c) { return c.addAll(FILES); })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) { return r || fetch(e.request); })
+  );
+});
